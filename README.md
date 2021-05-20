333f1f89de3678be64f5c9c504dda8ccdc0ba6ac
Munira Mohamed <muniramohamed@Muniras-MBP.broadband>
2021-05-20 11:24:40 +0100

    Control flow

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Control Flow
+### Control flow is very important in any language programming language to control the flow of your conditions and decisions
 
 - if, elif and else conditions
---
+++ control_flow.py
@@ -0,0 +1,12 @@
+# Control Flow with if, elif and else - loops
+
+weather = "sunny"
+
+if weather == "thinking": # if this condition is False execute the next line of code
+    print("Enjoy the weather") # if true this line will executed
+
+else:
+    print('Opps sorry something went wrong...please try later') # if false this line will be executed
+
+# add a condition to use elif when the condition is False
+
